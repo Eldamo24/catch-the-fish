7a8aa2a9a4bcf13fa23bb9bf13686195d6c81d8f
Damian Lobos <damianlobos24@gmail.com>
2024-01-12 09:34:26 -0300

    Score added

--- CatchTheFish/Assets/Scripts/ScoreManager.cs
+++ CatchTheFish/Assets/Scripts/ScoreManager.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     private int score;
+    private TextMeshProUGUI scoreTextUI;
     private void Awake()
     {
         instance = this;
+        scoreTextUI = GameObject.Find("ScoreTextUI").GetComponent<TextMeshProUGUI>();
     }
 
     public void AddScore()
     {
         score++;
-        Debug.Log("Sume puntos: " + score);
+        AddScoreUI();
+    }
+
+    private void AddScoreUI()
+    {
+        scoreTextUI.text = score.ToString();
     }
 }
